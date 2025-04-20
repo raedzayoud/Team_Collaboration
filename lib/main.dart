24527bec5b1eb45f3e949b16724368ca0fd68fba
Raed <raed.zayoud.23@gmail.com>
2025-04-20 11:33:27 +0200

    print

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ void main() async {
   options: DefaultFirebaseOptions.currentPlatform,
 );
   runApp(const MyApp());
+  print("Firebase initialized successfully");
 }
 
 class MyApp extends StatelessWidget {
