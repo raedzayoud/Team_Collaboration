122cbff415fb0d30d97d6710ace4345d2d0ef955
Raed <raed.zayoud.23@gmail.com>
2025-04-20 14:44:39 +0200

    add some validation of join mettings

--- lib/feature/meetings/presentation/view/joinmettings.dart
+++ lib/feature/meetings/presentation/view/joinmettings.dart
@@ -3,6 +3,7 @@ import 'package:collab_doc/feature/meetings/data/repos/metting_repos_impl.dart';
 import 'package:collab_doc/feature/meetings/presentation/manger/cubit/mettings_cubit.dart';
 import 'package:collab_doc/feature/meetings/presentation/view/widgets/apparjoinmettings.dart';
 import 'package:collab_doc/feature/meetings/presentation/view/widgets/textformfieldjoinmettings.dart';
+import 'package:collab_doc/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,13 +22,15 @@ class _JoinMeetingScreenState extends State<Joinmettings> {
   late final TextEditingController idController;
   late final TextEditingController nameController;
   final MettingReposImpl meetingRepositoryImplementation = MettingReposImpl();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     idController = TextEditingController();
     // nameController = TextEditingController(
     //     text: authRepositoryImplementation.user.displayName);
-    nameController = TextEditingController(text: "raed");
+    nameController = TextEditingController(
+        text: infoUserSharedPreferences.getString("username") ?? "");
 
     super.initState();
   }
@@ -43,59 +46,76 @@ class _JoinMeetingScreenState extends State<Joinmettings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ApparJoinMettings(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          TextFormFieldJoinMettings(
-            controller: idController,
-            hintText: 'Mettings ID',
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          TextFormFieldJoinMettings(
-            controller: nameController,
-            hintText: 'Name',
-          ),
-          const SizedBox(
-            height: 25.0,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(KPrimayColor),
-                shape: MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(13),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            TextFormFieldJoinMettings(
+              validator: (value) {
+                if (value!.length.toInt() < 4) {
+                  return 'Please enter a valid Mettings ID equal to 4 digits';
+                }
+                if (value.length.toInt() > 4) {
+                  return 'Please enter a valid Mettings ID less than 4 digits';
+                }
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Mettings ID';
+                }
+                return null;
+              },
+              controller: idController,
+              hintText: 'Mettings ID',
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextFormFieldJoinMettings(
+              controller: nameController,
+              hintText: 'Name',
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(KPrimayColor),
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(13),
+                      ),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    BlocProvider.of<MettingsCubit>(context)
+                        .joinMeeting(idController.text);
+                  }
+                },
+                child: const Center(
+                  child: Text(
+                    'Join',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
                     ),
                   ),
                 ),
               ),
-              onPressed: () {
-                BlocProvider.of<MettingsCubit>(context)
-                    .joinMeeting(idController.text);
-              },
-              child: const Center(
-                child: Text(
-                  'Join',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 17,
-                  ),
-                ),
-              ),
             ),
-          ),
-          const SizedBox(
-            height: 25.0,
-          ),
-        ],
+            const SizedBox(
+              height: 25.0,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/feature/meetings/presentation/view/widgets/textformfieldjoinmettings.dart
+++ lib/feature/meetings/presentation/view/widgets/textformfieldjoinmettings.dart
@@ -1,17 +1,20 @@
-
 import 'package:flutter/material.dart';
 
 class TextFormFieldJoinMettings extends StatelessWidget {
   final TextEditingController? controller;
   final String? hintText;
-  const TextFormFieldJoinMettings({super.key, this.controller, this.hintText});
+  final String? Function(String?)? validator;
+  const TextFormFieldJoinMettings(
+      {super.key, this.controller, this.hintText, this.validator});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: TextFormField(
+        validator: validator,
         controller: controller,
+        keyboardType: TextInputType.number,
         decoration: InputDecoration(
           hintText: hintText,
           hintStyle: const TextStyle(color: Colors.grey),
