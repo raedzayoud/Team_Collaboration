88beecbf790f4845932218a68c3c73d9ddb87df0
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2025-04-29 14:09:49 +0100

    add authgear

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => DocumentCubit(DocumentReposImpl())),
